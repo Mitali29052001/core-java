16811d84ab84f5b2e580e285c80733c6b17434b9
mitali <mitalikadam29052001@gmail.com>
2024-01-19 09:51:34 -1100

    first commit

---
+++ Looping Statements/UnderstaningWhile.java
@@ -0,0 +1,15 @@
+class UnderstaningWhile{
+	public static void main(String[] args) {
+	// 	System.out.println("start of main");
+	// 	while (false){
+	// 		System.out.println("from while loop");
+	// 	}
+	// 	System.out.println("End of main");
+	// }
+		System.out.println("start of main");
+		while (true){
+			System.out.println("from while loop");
+		}
+		// System.out.println("End of main");
+	}
+}
